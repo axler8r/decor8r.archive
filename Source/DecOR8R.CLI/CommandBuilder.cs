bacbe9c036e66e13afcf6d274fe5ea690c0d6255
axlER8R <axl.mattheus@gmail.com>
2020-10-29 11:16:03 +1300

    Update command builder

--- Source/DecOR8R.CLI/CommandBuilder.cs
+++ Source/DecOR8R.CLI/CommandBuilder.cs
@@ -1,4 +1,5 @@
 using System.CommandLine;
+using System.CommandLine.Invocation;
 
 namespace DecOR8R.CLI
 {
@@ -6,41 +7,70 @@ namespace DecOR8R.CLI
     {
         private readonly Command __command;
 
-        public CommandBuilder(Command command)
+        public CommandBuilder(string name) 
         {
-            __command = command;
+            // TODO: Add checks for arguments
+            __command = new Command(name);
+        }
+
+        public CommandBuilder(string name, string description) 
+        {
+            // TODO: Add checks for arguments
+            __command = new Command(name, description);
+        }
+
+        public CommandBuilder SetDescription(string description)
+        {
+            // TODO: Add checks for arguments
+            __command.Description = description;
+
+            return this;
+        }
+
+        public CommandBuilder SetHandler(ICommandHandler handler)
+        {
+            // TODO: Add checks for arguments
+            __command.Handler = handler;
+
+            return this;
         }
 
         public CommandBuilder AddAlias(string alias)
         {
+            // TODO: Add checks for arguments
             __command.AddAlias(alias);
 
             return this;
         }
 
-        public CommandBuilder AddArgument()
+        public CommandBuilder AddArgument<T>(Argument<T> argument)
         {
-            __command.AddArgument(null);
+            // TODO: Add checks for arguments
+            __command.AddArgument(argument);
 
             return this;
         }
 
-        public CommandBuilder AddCommand()
+        public CommandBuilder AddOption<T>(Option<T> option)
         {
-            __command.AddCommand(null);
+            // TODO: Add checks for arguments
+            __command.AddOption(option);
 
             return this;
         }
 
-        public CommandBuilder AddOption()
+        public CommandBuilder AddCommand(Command command)
         {
-            __command.AddOption(null);
+            // TODO: Add checks for arguments
+            __command.AddCommand(command);
 
             return this;
         }
 
-        public CommandBuilder AddValidator()
+        private CommandBuilder AddValidator()
         {
+            // TODO: Implement!
+            // TODO: Add checks for arguments
             __command.AddValidator(null);
 
             return this;
