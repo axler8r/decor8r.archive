afbdb052ac31f4b13ce4b6af14ab9ebd5ce64760
axlER8R <axl.mattheus@gmail.com>
2020-10-29 09:46:14 +1300

    Update factory

--- Source/DecOR8R.CLI/CommandFactory.cs
+++ Source/DecOR8R.CLI/CommandFactory.cs
@@ -2,9 +2,53 @@ using System.CommandLine;
 
 namespace DecOR8R.CLI
 {
-    public class CommandFactory {
+    public class CommandBuilder
+    {
         private readonly Command __command;
 
-        
+        public CommandBuilder(Command command)
+        {
+            __command = command;
+        }
+
+        public CommandBuilder AddAlias(string alias)
+        {
+            __command.AddAlias(alias);
+
+            return this;
+        }
+
+        public CommandBuilder AddArgument()
+        {
+            __command.AddArgument(null);
+
+            return this;
+        }
+
+        public CommandBuilder AddCommand()
+        {
+            __command.AddCommand(null);
+
+            return this;
+        }
+
+        public CommandBuilder AddOption()
+        {
+            __command.AddOption(null);
+
+            return this;
+        }
+
+        public CommandBuilder AddValidator()
+        {
+            __command.AddValidator(null);
+
+            return this;
+        }
+
+        public Command Build()
+        {
+            return __command;
+        }
     }
 }
