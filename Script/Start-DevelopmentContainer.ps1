be1bd159ee2bb3276dc8ca52630c981fb0edf849
axlER8R <axl.mattheus@gmail.com>
2020-12-02 12:38:38 +1300

    Toggle execution policy

--- Script/Start-DevelopmentContainer.ps1
+++ Script/Start-DevelopmentContainer.ps1
@@ -1,1 +1,11 @@
+$originalPolicy=Get-ExecutionPolicy
+if ($originalPolicy -ne "Bypass") {
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+}
+
 docker start decor8r.net
+
+$currentPolicy=Get-ExecutionPolicy
+if ($currentPolicy -ne $originalPolicy) {
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy $originalPolicy
+}
