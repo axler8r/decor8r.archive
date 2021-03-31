02c469f01165f8392826b41220ed789883c9601e
axlER8R <axl.mattheus@gmail.com>
2021-03-31 22:52:36 +1300

    Add shared object library

---
+++ Solution/DecOR8R.Common/IConfigurable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DecOR8R.Common
+{
+    /// <summary>
+    /// Configuration data.
+    /// </summary>
+    /// <typeparam name="K">Configuration key</typeparam>
+    /// <typeparam name="V">Conficuraiton value</typeparam>
+    public interface IConfigurable<K, V> where V : IConvertible
+    {
+        /// <summary>
+        /// Configuration data key-value table.
+        /// </summary>
+        public IDictionary<K, V> Configuration { get; set; }
+
+        /// <summary>
+        /// Retrieve a value for the specified key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public V Value(K key);
+    }
+}
---
+++ Solution/DecOR8R.Common/IDecorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DecOR8R.Common
+{
+    /// <summary>
+    /// Decorates subjects for the appropriate targtes.
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public interface IDecorator<K, V> where V: IConvertible
+    {
+        /// <summary>
+        /// Configuration data to use during the decoration process.
+        /// </summary>
+        public IConfigurable<K, V> Configuration { get; }
+
+        /// <summary>
+        /// Decorate the subject
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <returns></returns>
+        public string Decorate(IDecoratable subject);
+    }
+}
