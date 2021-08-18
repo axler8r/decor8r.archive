caa28378da652afc35235448f54d14a77cc2ff0f
AxlER8R <axl@axler8r.io>
2021-08-18 21:20:11 +0000

    Implement UNIX domain socket listener

--- Solution/DecOR8R.Daemon/Worker.cs
+++ Solution/DecOR8R.Daemon/Worker.cs
@@ -1,9 +1,11 @@
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace DecOR8R.Daemon
 {
@@ -11,35 +13,52 @@ namespace DecOR8R.Daemon
     {
         private readonly ILogger<Worker> _logger;
         private readonly IConfiguration _configuration;
+        private readonly string _socketFile;
 
-        public Worker(ILogger<Worker> logger, IConfiguration configuration)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration, string socket = "decor8r.sock")
         {
             _logger = logger;
             _configuration = configuration;
-        }
-
-        public override async Task StartAsync(CancellationToken cancellationToken)
-        {
-            await base.StartAsync(cancellationToken);
-        }
-
-        public override async Task StopAsync(CancellationToken cancellationToken)
-        {
-            await base.StopAsync(cancellationToken);
+            _socketFile = Path.Combine(Path.GetTempPath(), socket);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            if (File.Exists(_socketFile))
+            {
+               File.Delete(_socketFile); 
+            }
+
+            var address_ = new UnixDomainSocketEndPoint(_socketFile);
+            _logger.LogInformation($"Unix socket address: {address_}.");
+
+            var listener_ = new Socket(
+                addressFamily: AddressFamily.Unix,
+                socketType: SocketType.Stream,
+                protocolType: ProtocolType.Unspecified);
+            listener_.Bind(address_);
+            listener_.Listen();
+            _logger.LogInformation($"Started listner: {listener_.ToString()}.");
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
-        }
+                _logger.LogInformation("Ready to accept requests...");
+                var socket_ = listener_.Accept();
+                _logger.LogInformation($"Accepped request: {socket_.ToString()}.");
 
-        public override void Dispose()
-        {
-            base.Dispose();
+                var buffer_ = new byte[1024];
+                var requestSize_ = socket_.Receive(
+                    buffer: buffer_,
+                    offset: 0,
+                    size: buffer_.Length,
+                    socketFlags: SocketFlags.None);
+                var request_ = Encoding.UTF8.GetString(buffer_, 0, requestSize_);
+                _logger.LogInformation($"Receved: {request_}");
+
+                socket_.Close();
+
+                await Task.Delay(0, stoppingToken);
+            }
         }
     }
 }
