fbcdf9240be5088a790e3d87e6ac96fb9ee1415c
AxlER8R <axl@axler8r.io>
2021-09-11 11:00:46 +1200

    Implement https://github.com/axler8r/decor8r/issues/1

--- Solution/DecOR8R.CLI/Program.cs
+++ Solution/DecOR8R.CLI/Program.cs
@@ -1,75 +1,42 @@
+using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
 using System.Threading.Tasks;
 
-// using Serilog;
-
 namespace DecOR8R.CLI
 {
     internal partial class Program
     {
-        // private static readonly ILogger _logger = new LoggerConfiguration()
-        //    .WriteTo.File("decor8r.log")
-        //    .CreateLogger();
-
-        // public static ILogger Logger => _logger;
 
         private static async Task<int> Main(params string[] args)
         {
-            // Logger.Information("About to enter decor8r");
-
-            var decor8r_ = new RootCommand("decor8r")
-            {
-                new CommandBuilder("configure")
-                    .SetDescription("Configure decorator")
-                    .AddAlias("config")
-                    .AddCommand(new CommandBuilder("initialize")
-                        .SetDescription("Create a default configuration file")
-                        .AddAlias("init")
-                        .SetHandler(CommandHandler.Create(
-                            () =>
-                            {
-                                var config_ = Configurator.GetConfiguration();
-                                var json_ = System.Text.Json.JsonSerializer.Serialize(config_);
-                                System.Console.WriteLine(json_);
-                            }))
-                        .Build())
+            var decor8r_ = new RootCommand() {
+                new OptionBuilder<FileInfo>("--configuration")
+                    .AddAlias("--config")
+                    .SetRequried(false)
                     .Build(),
-                new CommandBuilder("decorate")
-                    .AddCommand(new CommandBuilder("terminal")
-                        .AddOption<int>(new OptionBuilder<int>(
-                                name: "--width",
-                                defaultValue: () => System.Console.WindowWidth)
-                            .SetDescription("Terminal width")
-                            .AddAlias("-w")
-                            .Build())
-                        .AddOption<TerminalType>(new OptionBuilder<TerminalType>(
-                                name: "--type",
-                                defaultValue: () => TerminalType.ANSI)
-                            .SetDescription("Type of terminal")
-                            .AddAlias("-t")
-                            .Build())
-                        .AddArgument<DirectoryInfo>(new ArgumentBuilder<DirectoryInfo>(
-                                name: "path")
-                            .SetDescription("Path to decorate")
-                            .SetArity(ArgumentArity.ExactlyOne)
-                            .Build())
-                        .SetHandler(CommandHandler.Create<DirectoryInfo, TerminalDecorationOptions>(
-                            (DirectoryInfo path, TerminalDecorationOptions tdos) =>
-                            {
-                                var configuration_ = Configurator.GetConfiguration();
-                                TerminalDecorator.Decorate(path, tdos, configuration_);
-                            }))
-                        .Build())
-                    .AddCommand(new CommandBuilder("neovim")
-                        .Build())
-                    .AddCommand(new CommandBuilder("tmux")
-                        .Build())
-                    .Build(),
+                new ArgumentBuilder<String>(
+                    name: "path",
+                    description: "Path to decorate")
+                    .Build()
             };
-
-            // Logger.Information("About to exit decor8r");
+            decor8r_.Handler = CommandHandler.Create<FileInfo, String>((configuration, path) =>
+            {
+                if (configuration != null && !configuration.Exists)
+                {
+                    System.Console.WriteLine(String.Format(
+                        "{0}\n\t{1}\n\t{2}\n\t{3}",
+                        "Error! Cannot load configuration file (ECFG000).",
+                        "To learn more, run: decor8r describe ECFG000.",
+                        "To create a valid configuration file run: decor8r config init.",
+                        "For more information visit https://github.com/axler8r/decor8r/wiki/Configuration."));
+                }
+                else
+                {
+                    System.Console.WriteLine(path);
+                }
+            });
 
             return await decor8r_.InvokeAsync(args);
         }
