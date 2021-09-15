f14219fbdda817d1559bd68a6bf009fa6454735a
AxlER8R <axl@axler8r.io>
2021-09-15 15:18:20 +1200

    Add services

---
+++ Solution/DecOR8R.Daemon/Services/LoggingService.cs
@@ -0,0 +1,34 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+
+namespace DecOR8R.Daemon
+{
+    public class LoggingService : BackgroundService
+    {
+        public LoggingService()
+        {
+        }
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            Log.Information($"Starting {this.GetType().Name}...");
+            return base.StartAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(30000, stoppingToken);
+            }
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            Log.Information($"Stopping {this.GetType().Name}...");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
