0b528a60d5268fbb472027d557631b3ee0ac9c7a
AxlER8R <axl@axler8r.io>
2021-09-15 15:15:11 +1200

    Add Serilog logger
    Signed-off-by: AxlER8R <axl@axler8r.io>

--- Solution/DecOR8R.Daemon/Program.cs
+++ Solution/DecOR8R.Daemon/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace DecOR8R.Daemon
 {
@@ -7,12 +8,26 @@ namespace DecOR8R.Daemon
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                //.Enrich.FromLogContext()
+                #if DEBUG
+                .WriteTo.Console()
+                #endif
+                .WriteTo.File(
+                    path: "log.log",
+                    rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .UseSystemd()
+                .ConfigureLogging((context, loggers) => {
+                })
+                .UseSerilog()
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
