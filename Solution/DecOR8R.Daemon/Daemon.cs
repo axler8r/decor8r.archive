1cc2fe8ad8c66ca48eed06eff443921e56b0a4ed
AxlER8R <axl@axler8r.io>
2021-09-16 15:56:47 +1200

    Tweak logger settings
    Signed-off-by: AxlER8R <axl@axler8r.io>

--- Solution/DecOR8R.Daemon/Daemon.cs
+++ Solution/DecOR8R.Daemon/Daemon.cs
@@ -9,17 +9,28 @@ namespace DecOR8R.Daemon
 {
     public class Program
     {
+        // TODO: Move to configuration file
+        private const string FILE_LOG_FORMAT =
+            "[{Timestamp:yyyy-MM-ddTHH:mm:ss.ffK} {Level:u4}] " +
+            "({SourceContext}) " +
+            "{Message}{NewLine}{Exception}";
+
+        // TODO: Move to configuration file
+        private const string CONSOLE_LOG_FORMAT =
+            "[{Level:u4}] ({SourceContext}) {Message}{NewLine}{Exception}";
+
         public static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Information()
-                //.Enrich.FromLogContext()
-                #if DEBUG
-                .WriteTo.Console()
-                #endif
+                .Enrich.FromLogContext()
+#if DEBUG
+                .WriteTo.Console(outputTemplate: CONSOLE_LOG_FORMAT)
+#endif
                 .WriteTo.File(
-                    path: "log.log",
-                    rollingInterval: RollingInterval.Day)
+                    path: "decor8r.log",
+                    rollingInterval: RollingInterval.Day,
+                    outputTemplate: FILE_LOG_FORMAT)
                 .CreateLogger();
 
             try
