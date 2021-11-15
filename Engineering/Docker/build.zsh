490d85fc10a32be3200c35557437725c1dba5bc5
AxlER8R <axl@axler8r.io>
2021-11-16 11:58:37 +1300

    feat: implement docker autotagging

--- Engineering/Docker/build.zsh
+++ Engineering/Docker/build.zsh
@@ -1,8 +1,24 @@
 #!/usr/bin/env zsh
 
-# TODO: Define PRODUCT and VERSION
-HASH=$(cat /dev/urandom | tr -dc '[[:alnum:]]' | head -c 7)
+PRODUCT="Unknown"
+MAJOR=0
+MINOR=0
+PATCH=0
+
+if [[ ! -f ./TAG ]] {
+    printf "%s\n" "Missing TAG file, using defaults"
+} else {
+    PRODUCT=$(cat TAG | grep PRODUCT | sed -e 's/PRODUCT=//')
+    MAJOR=$(cat TAG | grep MAJOR | sed -e 's/MAJOR=//')
+    MINOR=$(cat TAG | grep MINOR | sed -e 's/MINOR=//')
+    PATCH=$(cat TAG | grep PATCH | sed -e 's/PATCH=//')
+}
+
+[[ "${PATCH}" == "auto" ]] && PATCH=$(date +%H%M%S)
+TAG="${PRODUCT}:${MAJOR}.${MINOR}.${PATCH}"
 if [[ -x $(which docker) ]] {
-	docker build --tag "${PRODUCT:-decor8r}:${VERSION:-$(date +f %y%m%d)}-${HASH}"
+	docker build --tag "${TAG}" .
+} else {
+    printf "%s\n" "Docker not installed"
 }
 
