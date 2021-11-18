bb01150b386a709213ba84e85ca0ac8004a5619b
AxlER8R <axl@axler8r.io>
2021-11-18 23:25:52 +1300

    refactor: apply file scope namespace
    Signed-off-by: AxlER8R <axl@axler8r.io>

--- Solution/DecOR8R.CLI/Program.cs
+++ Solution/DecOR8R.CLI/Program.cs
@@ -1,3 +1,5 @@
+namespace DecOR8R.CLI;
+
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
@@ -6,95 +8,93 @@ using System.IO;
 using System.Net.Sockets;
 using System.Text;
 
-namespace DecOR8R.CLI
+internal static class Program
 {
-    internal static class Program
+    private static readonly IDictionary<string, string> Payload = new Dictionary<string, string>();
+
+    private static int Main(params string[] args)
     {
-        private static readonly IDictionary<string, string> Payload = new Dictionary<string, string>();
-
-        private static int Main(params string[] args)
+        var decor8R = new RootCommand
         {
-            var decor8R = new RootCommand
+            Decorate()
+        };
+
+        var invoke = decor8R.Invoke(args);
+        if (invoke != 0) return invoke;
+
+        var payload = new StringBuilder();
+        foreach (var key in Payload.Keys) payload.AppendLine($"{key}={Payload[key]}");
+
+        return Call(payload.ToString());
+    }
+
+    private static Command Decorate()
+    {
+        return new Command("decorate")
+        {
+            Terminal(),
+            Tmux(),
+            NeoVim()
+        };
+    }
+
+    private static Command Terminal()
+    {
+        var result = new Command("terminal")
+        {
+            new Option<int>(
+                new[] {"-w", "--width"},
+                "Terminal width"),
+            new Option<string>(
+                new[] {"-p", "--path"},
+                "Path to decorate")
+        };
+        result.Handler = CommandHandler.Create<int, string>(
+            (width, path) =>
             {
-                Decorate()
-            };
+                Payload.Add("PAYLOAD", "TERMINAL");
+                Payload.Add("width", width.ToString());
+                Payload.Add("path", path);
+            });
 
-            var invoke = decor8R.Invoke(args);
+        return result;
+    }
 
-            var payload = new StringBuilder();
-            foreach (var key in Payload.Keys) payload.AppendLine($"{key}={Payload[key]}");
+    private static Command Tmux()
+    {
+        return new Command("tmux");
+    }
 
-            return Call(payload.ToString());
-        }
+    private static Command NeoVim()
+    {
+        return new Command("neovim");
+    }
 
-        private static Command Decorate()
+    private static int Call(string path)
+    {
+        try
         {
-            return new Command("decorate")
-            {
-                Terminal(),
-                Tmux(),
-                NeoVim()
-            };
-        }
+            var address = Path.Combine(Path.GetTempPath(), "decor8r.sock");
+            var endpoint = new UnixDomainSocketEndPoint(address);
+            using var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+            socket.Connect(endpoint);
 
-        private static Command Terminal()
+            var requestBytes = Encoding.UTF8.GetBytes(path);
+            var request = Encoding.UTF8.GetString(requestBytes);
+            socket.Send(requestBytes);
+
+            var responseBytes = new byte[1024];
+            var responseSize = socket.Receive(responseBytes);
+            var response = Encoding.UTF8.GetString(responseBytes, 0, responseSize);
+
+            Console.WriteLine($"RX ---------\n{request}TX ---------\n{response}");
+        }
+        catch (Exception e)
         {
-            var result = new Command("terminal")
-            {
-                new Option<int>(
-                    new[] {"-w", "--width"},
-                    "Terminal width"),
-                new Option<string>(
-                    new[] {"-p", "--path"},
-                    "Path to decorate")
-            };
-            result.Handler = CommandHandler.Create<int, string>(
-                (width, path) =>
-                {
-                    Payload.Add("PAYLOAD", "TERMINAL");
-                    Payload.Add("width", width.ToString());
-                    Payload.Add("path", path);
-                });
-
-            return result;
+            Console.WriteLine(e);
+            return 1;
         }
 
-        private static Command Tmux()
-        {
-            return new Command("tmux");
-        }
-
-        private static Command NeoVim()
-        {
-            return new Command("neovim");
-        }
-
-        private static int Call(string path)
-        {
-            try
-            {
-                var address = Path.Combine(Path.GetTempPath(), "decor8r.sock");
-                var endpoint = new UnixDomainSocketEndPoint(address);
-                using var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
-                socket.Connect(endpoint);
-
-                var requestBytes = Encoding.UTF8.GetBytes(path);
-                var request = Encoding.UTF8.GetString(requestBytes);
-                socket.Send(requestBytes);
-
-                var responseBytes = new byte[1024];
-                var responseSize = socket.Receive(responseBytes);
-                var response = Encoding.UTF8.GetString(responseBytes, 0, responseSize);
-
-                Console.WriteLine($"RX ---------\n{request}TX ---------\n{response}");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return 1;
-            }
-
-            return 0;
-        }
+        return 0;
     }
 }
