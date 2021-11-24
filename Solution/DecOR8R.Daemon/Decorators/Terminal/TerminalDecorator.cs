d49b85e4b27fb6fd9f28a79dc2ea31d3c096c048
AxlER8R <axl@axler8r.io>
2021-11-24 22:50:16 +1300

    style: change member visibility
    Signed-off-by: AxlER8R <axl@axler8r.io>

--- Solution/DecOR8R.Daemon/Decorators/Terminal/TerminalDecorator.cs
+++ Solution/DecOR8R.Daemon/Decorators/Terminal/TerminalDecorator.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 using static System.CommandLine.Rendering.Ansi;
 
 namespace DecOR8R.Daemon.Decorators.Terminal;
@@ -6,15 +8,15 @@ class TerminalDecorator : IDecorator<string>
 {
     private static readonly ILogger Log = Serilog.Log.ForContext<TerminalDecorator>();
 
-    internal string Decorate(string path)
+    public string Decorate(string subject)
     {
-        if (string.IsNullOrWhiteSpace(path))
+        if (string.IsNullOrWhiteSpace(subject))
         {
-            throw new System.ArgumentException($"'{nameof(path)}' cannot be null or whitespace.", nameof(path));
+            throw new System.ArgumentException($"'{nameof(subject)}' cannot be null or whitespace.", nameof(subject));
         }
 
-        var result = path.Replace("/", "  ");
-        result = $"{Base16.Foreground.Base2}{Base16.Background.Blue}{result}{Color.Background.Default}{Color.Foreground.Default}{Base16.Foreground.Blue} {Base16.Foreground.Default}";
+        var result = subject.Replace("/", "  ");
+        result = $"{Base16.Foreground.Base2}{Base16.Background.Blue}{result} {Color.Background.Default}{Color.Foreground.Default}{Base16.Foreground.Blue}{Base16.Foreground.Default}";
 
         return result;
     }
